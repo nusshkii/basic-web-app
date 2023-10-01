7cfd02e3e3e73e4aab5dad15473f4924f7610bb1
Anushka <asatpute@andrew.cmu.edu>
2023-10-01 12:31:09 +0300

    added test

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -17,4 +17,12 @@ describe("QueryProcessor", () => {
             "writer in the English language and the world's pre-eminent dramatist."
           ));
     });
+
+    test('should return andrewID', () => {
+        const query = "andrewID";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "asatpute"
+          ));
+    });
 });
