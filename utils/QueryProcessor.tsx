27672cca9e7af36c36be061d73aff511dfdd5200
mrahmatu <mrahmatu@andrew.cmu.edu>
2023-10-05 12:33:19 +0300

    lessgo

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -48,6 +48,13 @@ export default function QueryProcessor(query: string): string {
     return (x+y).toString();
   }
 
+  const multiplyMatch = query.toLowerCase().match(/what is (\d+) multiplied by (\d+)/);
+  if (multiplyMatch) {
+    const x: number = parseInt(multiplyMatch[1]);
+    const y: number = parseInt(multiplyMatch[2]);
+    return (x*y).toString();
+  }
+
 
   
  
